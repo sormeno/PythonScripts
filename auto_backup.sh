949a4682b2223bd4a266d57e98017d85b217b313
Filip <filiptrn40@gmail.com>
2024-04-17 23:31:31 +0200

    BACKUP 2024-04-17

--- auto_backup.sh
+++ auto_backup.sh
@@ -2,7 +2,7 @@ git add .
 #create commit and push only if there are files staged
 count=$(git diff --cached --name-only | wc -l)
 if [ "$count" -gt 0 ]; then
-    commit_message=$(date +'%Y-%m-%d %H:%M:%S')
+    commit_message=$(date +'%Y-%m-%d')
     git commit -m "BACKUP $commit_message"
     git push
 fi
